6ec32c73b0869abfd6953ebf208eb9b162cc3776
Renzel <renzel.david@gmail.com>
2022-03-24 16:04:16 +0000

    allow users to register accounts

--- users/urls.py
+++ users/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     url(r'^login/$', LoginView.as_view(template_name='users/login.html'), name='login'),
     # Logout page
     url(r'^logout/$', views.logout_view, name='logout'),
+    # Registration page
+    url(r'^register/$', views.register, name='register'),
 ]
