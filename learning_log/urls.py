48739386e1971543fca19972a84a1e04ff49c4c2
Renzel <renzel.david@gmail.com>
2022-03-24 14:09:28 +0000

    make users app and create a login page

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -18,6 +18,7 @@ from django.urls import re_path as url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^users/', include('users.urls', namespace='users_app')),
     url(r'', include('learning_logs.urls', namespace='learning-logs')),
 ]
---
+++ users/urls.py
@@ -0,0 +1,16 @@
+"""Defines URL patterns for users"""
+
+
+from django.urls import include
+from django.urls import re_path as url
+from django.contrib.auth.views import LoginView
+
+from . import views
+
+
+app_name = 'users'
+
+urlpatterns = [
+    # Login page
+    url(r'^login/$', LoginView.as_view(template_name='users/login.html'), name='login')
+]
