4f5bdccc28b7a360202d67903c2064b686a6f5f8
renda28 <renzel.david@gmail.com>
2022-03-27 15:24:34 +0100

    custom error pages

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'learning_log/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
